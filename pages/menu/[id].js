4abc3f4e537745035ceda81998c9c69e4166475a
sajad Afkhami <sajad.afkhami10@gmail.com>
2025-06-10 13:34:43 +0330

    Fetch data by dynamic routes

---
+++ pages/menu/[id].js
@@ -0,0 +1,48 @@
+import { useRouter } from 'next/router'
+import React from 'react'
+
+function Details({data}) {
+    const router = useRouter();
+
+    if(router.isFallback){
+        return <h2>Loading Page...</h2>
+    }
+  return (
+    <div>Details</div>
+  )
+}
+
+export default Details
+
+export async function getStaticPaths() {
+    const res = await fetch("http://localhost:4000/data")
+    const json = await res.json()
+    const data = json.slice(0, 10);
+
+    const paths = data.map(food => ({params: {id: food.id.tostring()}}))
+
+    return {
+        paths: paths,
+        fallback: true,
+    }
+}
+
+export async function getStaticProps(context) {
+    const {params} = context;
+    const res = await fetch(`http://localhost:4000/data/${params.id}`);
+    const data = await res.json()
+    console.log(data)
+    if(!data.id){
+        return {
+            notFound: true,
+        }
+    }
+
+    return {
+        props: {
+            data: data
+        },
+        revalidate: 10,
+        
+    }
+}
