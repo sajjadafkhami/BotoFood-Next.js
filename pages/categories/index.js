2e44aad14b2c8bd59424fccece3bd16a30225dc4
sajad Afkhami <sajad.afkhami10@gmail.com>
2025-06-11 15:26:28 +0330

    data filtering with server side rendering

--- pages/categories/index.js
+++ pages/categories/index.js
@@ -1,12 +1,49 @@
 import React from 'react'
 import CategoriesPage from '../../components/templates/CategoriesPage'
 
-function Categories() {
+function Categories({ data }) {
+    console.log(data)
   return (
     <div>
-        <CategoriesPage />
+        <CategoriesPage data={data}/>
     </div>
   )
 }
 
 export default Categories
+
+export async function getServerSideProps(context) { //use context to get query
+ const {query:{ difficulty, time }} = context
+
+ const res = await fetch("http://localhost:4000/data");
+ const data = await res.json();
+
+ const filteredData = data.filter((item) => {
+    const difficultyResult = item.details.filter
+    (detail => detail.Difficulty && detail.Difficulty === difficulty);
+
+    const timeResult = item.details.filter(detail => {
+        const cookingTime = detail["Cooking Time"] || "";
+        const timeDetail = cookingTime.split(" ")[0];
+        if(time === "less" && timeDetail && +timeDetail <= 30){
+            return detail;
+        } else if (time === "more" && timeDetail > 30){
+            return detail;
+        }
+    });
+    if (time && difficulty && timeResult.length && difficultyResult.length){
+        return item
+    } else if (!time && difficulty && difficultyResult.length) {
+        return item;
+    } else if ( time && !difficulty && timeResult.length) {
+        return item;
+    }
+ });
+
+ return {
+    props: {
+        data: filteredData
+    },
+ };
+
+}
